0db9f6b92da3bac411c37ef451404d978ebc0f8f
Aditya Mankotia <adityamankotia20@gmail.com>
2024-10-11 23:14:32 +0530

    Dynamic Watchlist

--- movies-app/src/Components/Watchlist.jsx
+++ movies-app/src/Components/Watchlist.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Watchlist = () => {
+  const [watchlist,setWatchlist] =useState([])
+  useEffect( ()=> {
+    let moviesFromLs=localStorage.getItem('movies')
+    if(moviesFromLs){
+      setWatchlist(JSON.parse(moviesFromLs))
+    }
+  },[])
   return (
     <div className="overflow-hidden rounded-lg border border-gray-200 shadow-md m-5">
         <table className="w-full border-collapse bg-white text-left text-sm
@@ -26,15 +33,18 @@ const Watchlist = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-gray-100 border-t border-gray-100">
-              <tr className="hover:bg-gray-50">
-                  <td className="flex items-center px-6 py-4 font-normal text-gray-900">
-                    <img className="h-[6rem] w-[10rem] object-fit" src="" alt="" />
-                    <div className="font-medium text-gray-700 text-sm">Star Wars</div>
-                  </td>
-                  <td className="pl-6 py-4">7.8</td>
-                  <td className="pl-6 py-4">7.8</td>
-                  <td className="pl-2 py-4">Action</td>
-                </tr>
+            {watchlist.map((movie)=> {
+              return(<tr className="hover:bg-gray-50">
+              <td className="flex items-center px-6 py-4 font-normal text-gray-900">
+                <img className="h-[6rem] w-[10rem] object-fit" src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} />
+                <div className="font-medium text-gray-700 text-sm">{movie.title}s</div>
+              </td>
+              <td className="pl-6 py-4">{movie.vote_average}</td>
+              <td className="pl-6 py-4">{movie.popularity}</td>
+              <td className="pl-2 py-4">Action</td>
+            </tr>)
+            })}
+                
           </tbody>
         </table>
       </div>
